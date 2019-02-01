6d4c5f2f9391ab16de1210dcce9a4ad8fa3dea33
matses332 <kristoffer332@hotmail.com>
2019-02-01 13:11:30 +0100

    latest

--- src/components/app.router.jsx
+++ src/components/app.router.jsx
@@ -5,12 +5,14 @@ import pictureLibrary from './picturelibrary.component';
 import MixesComponent from './mixes.component';
 import ContactComponent from './contact.component';
 import FeaturedPicturesComponent from './featuredpictures.component';
+import InformationComponent from './information.component';
 
 
 const AppRouter = () => (
     <Router>
         <div>
             <Route path='/' exact component={FeaturedPicturesComponent} />
+            <Route path ='/' exact component={InformationComponent}/>
             <Route path='/Gallery' exact component={pictureLibrary} />
             <Route path='/Mixes' exact component={MixesComponent} />
             <Route path='/Contact' exact component={ContactComponent}/>
